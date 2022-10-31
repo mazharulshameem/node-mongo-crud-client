ed4059918d957d7ae1d34a641ab09e98db471874
Mazharul Shameem <mazharulis.cs.07@gmail.com>
2022-10-31 15:58:56 +0600

    data added

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,13 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddUser = () => {
+  const [user, setUser] = useState({});
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    console.log(user);
+
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.acknowledged) {
+          alert(" User Added Successfully");
+          event.target.reset();
+        }
+      });
+  };
+  const handleBlur = (event) => {
+    const field = event.target.name;
+    const value = event.target.value;
+    const newUser = { ...user };
+    newUser[field] = value;
+    setUser(newUser);
+  };
+
   return (
     <div>
       <h1>Please Add a New User</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="name" placeholder="Name" />
+      <form onSubmit={handleAddUser}>
+        <input
+          onBlur={handleBlur}
+          type="text"
+          name="name"
+          placeholder="Name"
+          required
+        />
         <br />
-        <input type="email" name="email" placeholder="Email" />
+        <input
+          onBlur={handleBlur}
+          type="text"
+          name="address"
+          placeholder="Address"
+          required
+        />
+        <br />
+        <input
+          onBlur={handleBlur}
+          type="email"
+          name="email"
+          placeholder="Email"
+          required
+        />
         <br />
         <input type="submit" value="AddUser" />
       </form>
