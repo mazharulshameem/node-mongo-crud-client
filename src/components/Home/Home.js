b48859a400e90ccd821e389f3c24f2d98abb3549
Mazharul Shameem <mazharulis.cs.07@gmail.com>
2022-10-31 17:22:11 +0600

    delete section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 const Home = () => {
+  const users = useLoaderData();
+  const [displayUsers, setDisplayUsers] = useState(users);
+  const handleDelete = (user) => {
+    const agree = window.confirm(` Are You sure to delete? ${user.name}`);
+    if (agree) {
+      // console.log("deleting", user._id);
+      fetch(`http://localhost:5000/users/${user._id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.deletedCount > 0) {
+            alert(" User deleted successfully");
+            const remainingUsers = displayUsers.filter(
+              (usr) => usr._id !== user._id
+            );
+            setDisplayUsers(remainingUsers);
+          }
+        });
+    }
+  };
   return (
     <div>
-      <h1>User</h1>
+      <h1>User: {displayUsers.length}</h1>
+      <div>
+        {displayUsers.map((user) => (
+          <p key={user._id}>
+            {user.name} {user.email}{" "}
+            <button onClick={() => handleDelete(user)}>X</button>
+          </p>
+        ))}
+      </div>
     </div>
   );
 };
